0079f5f1feda4a98397d0ca111d231d6605f1c0a
mikzanco <mikyzancolo97@gmail.com>
2022-10-18 10:31:21 +0200

    fix loop infinito

--- js/script.js
+++ js/script.js
@@ -10,18 +10,21 @@ let imagesTags = '';
 
 const slider = document.querySelector('.items-wrapper');
 
+const next = document.querySelector('.down');
+const prev = document.querySelector('.top');
 
-for(let i = 0; i < imagesArray.length; i++){
+const numImages = 5;
+
+let counterImages = 0;
+
+
+for(let i = 0; i <= imagesArray.length; i++){
     
     imagesTags += `
         <img class="item" src="img/${imagesArray[i]}" alt="">
     `;
     
 }
-
-let counterImages = 0;
-const next = document.querySelector('.down');
-const prev = document.querySelector('.top');
 
 
 slider.innerHTML += imagesTags;
@@ -32,25 +35,31 @@ const items = document.getElementsByClassName('item');
 items[counterImages].classList.add('active');
 
 next.addEventListener('click', function() {
+
     items[counterImages].classList.remove('active');
 
-    items[++counterImages].classList.add('active');
+    counterImages++;
 
+    
 
-    if(counterImages === imagesArray.length -1){
-        counterImages = 0;
-        items[0].classList.add('active');
-    }
+    
+
+    if(counterImages === imagesArray.length ) counterImages = 0;
+        
+    items[counterImages].classList.add('active');
+    
     
 })
 
 prev.addEventListener('click', function() {
     items[counterImages].classList.remove('active');
 
-    items[--counterImages].classList.add('active');
+    counterImages--;
 
-    if(counterImages === imagesArray.length -1){
-        counterImages = 5;
-        items[5].classList.add('active');
-    }
+    
+
+    if(counterImages < 0) counterImages = numImages;
+
+    items[counterImages].classList.add('active');
+    
 })
